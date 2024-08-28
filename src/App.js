34ac2be73365c43f0cf183d242710ab7ffb6fe7c
JohnsonNYC <jkow95@gmail.com>
2024-08-28 15:29:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Suspense, lazy } from "react";
+
+const Spline = lazy(() => import("@splinetool/react-spline"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="min-h-screen">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline scene="https://prod.spline.design/9J3K5TNzaDjebq7Z/scene.splinecode" />
+      </Suspense>
+    </main>
   );
 }
 
